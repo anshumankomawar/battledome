1ad994a3a58074034a0208f1dc4691e69d829407
Anshuman Komawar <anshumankomawar@gmail.com>
2022-03-19 02:05:39 -0700

    Added pointerlockcontrols

--- src/initThree.js
+++ src/initThree.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls.js';
 
 let container;
 let camera, scene, renderer;
@@ -11,12 +12,18 @@ const initThree = () => {
   scene = new THREE.Scene();
 
   camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 20);
-  camera.position.z = 5;
 
   const geometry = new THREE.BoxGeometry();
   const material = new THREE.MeshBasicMaterial({ color: 0xFFFFFF });
   cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  //scene.add(cube);
+
+  const geometry2 = new THREE.PlaneGeometry(10, 10);
+  const material2 = new THREE.MeshBasicMaterial( {color: 0xFFFFFF, side: THREE.DoubleSide} );
+  const plane = new THREE.Mesh( geometry2, material2 );
+  plane.rotation.x = - Math.PI / 2;
+  plane.position.y = -1;
+  scene.add( plane );
 
   const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
   light.position.set(0.5, 1, 0.25);
@@ -28,21 +35,35 @@ const initThree = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   container.appendChild(renderer.domElement);
 
+  let keys = [];//Define array
+
+  window.addEventListener('keydown',keydown);
+  window.addEventListener('keyup',keyup);
+  //Attach listeners to functions
+
+  function keydown(e){ keys[e.key] = true; }
+
+  function keyup(e){ keys[e.key] = false; }
+
+  const controls = new PointerLockControls( camera, renderer.domElement );
   window.addEventListener("resize", onWindowResize, false);
+  window.addEventListener( 'click', function () {controls.lock();}, false );
 
   const render = (timestamp, frame) => {
-    renderer.render(scene, camera);
+    if(keys['w']) controls.moveForward(.1);
+    if(keys['s']) controls.moveForward(-.1);
+    if(keys['a']) controls.moveRight(-.1);
+    if(keys['d']) controls.moveRight(.1);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
   };
 
   const animate = () => {
     renderer.setAnimationLoop(render);
 
     // // if using RequestAnimation()
-    // requestAnimationFrame(animate)
-    // render()
+    //requestAnimationFrame(animate)
+    //render()
   };
   animate();
 };
