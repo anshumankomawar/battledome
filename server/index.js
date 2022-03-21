c63e32bf619d6100977d1dd1b3faf550155c2254
Anshuman Komawar <anshumankomawar@gmail.com>
2022-03-20 17:30:33 -0700

    Node webpack test

--- server/index.js
+++ server/index.js
@@ -1,19 +1,33 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+import path from 'path'
+import express from 'express'
+const app = express(),
+            DIST_DIR = __dirname,
+            HTML_FILE = path.join(DIST_DIR, 'index.html')
+app.use(express.static(DIST_DIR))
+app.get('*', (req, res) => {
+    res.sendFile(HTML_FILE)
+})
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`App listening to ${PORT}....`)
+    console.log('Press Ctrl+C to quit.')
+})
+//const express = require('express');
+//const app = express();
+//const http = require('http');
+//const server = http.createServer(app);
+//const { Server } = require("socket.io");
+//const io = new Server(server);
 
-app.get('/', (req, res) => {
-  console.log('GET /');
+//app.get('/', (req, res) => {
+  //console.log('GET /');
   //res.sendFile(__dirname + '/index.html');
-});
+//});
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
+//io.on('connection', (socket) => {
+  //console.log('a user connected');
+//});
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
+//server.listen(3000, () => {
+  //console.log('listening on *:3000');
+//});
